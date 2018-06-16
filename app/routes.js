f15ac07c846e19b685285524e6dc54524ac9149d
k <lookerkj@protonmail.com>
2018-06-15 17:54:01 -0700

    in order to test the second end, had to add a new:true to return the new user information and had to take islooged in off the route file

--- app/routes.js
+++ app/routes.js
@@ -6,32 +6,32 @@ const jsonParser = bodyparser.json();
 
 
 
-module.exports = function(app, passport) {
+module.exports = function (app, passport) {
 
-//=================================================================
-// HOME PAGE WITH LOGIN LINK ======================================
-//=================================================================
-  app.get('/', function(req, res) {
+  //=================================================================
+  // HOME PAGE WITH LOGIN LINK ======================================
+  //=================================================================
+  app.get('/', function (req, res) {
     res.render('index.ejs'); //load the index.ejs file
   });
-  
+
   //================================================================
   //LOGIN ==========================================================
   //================================================================
 
   //SHOW THE LOGIN FORM===============
-  app.get('/login', function(req, res) {
+  app.get('/login', function (req, res) {
 
     //render the page and pass in any flash data if it exists 
 
-    res.render('login.ejs', {message: req.flash('loginMessage')});
+    res.render('login.ejs', { message: req.flash('loginMessage') });
   });
-    
+
   // process the login form
   app.post('/login', passport.authenticate('local-login', {
-    successRedirect : '/profile', //redirect to secure profile section
-    failureRedirect : '/login', //redirect back to login
-    failureFlash : true // allow flash messages EDIT
+    successRedirect: '/profile', //redirect to secure profile section
+    failureRedirect: '/login', //redirect back to login
+    failureFlash: true // allow flash messages EDIT
   }));
 
 
@@ -39,12 +39,12 @@ module.exports = function(app, passport) {
   // SIGNUP ==============================
   // =====================================
 
-  app.get('/signup', function(req, res){
+  app.get('/signup', function (req, res) {
     // render the page and pass in any flash data if it exists
-    res.render('signup.ejs', {message: req.flash('signupMessage')});
+    res.render('signup.ejs', { message: req.flash('signupMessage') });
   });
-  
-  app.get('/data', isLoggedIn, function(req,res) {
+
+  app.get('/data', isLoggedIn, function (req, res) {
     console.log(req.cookies);
     res.send('ok');
   });
@@ -52,9 +52,9 @@ module.exports = function(app, passport) {
 
   // process the signup form
   app.post('/signup', passport.authenticate('local-signup', {
-    successRedirect : '/profile', //redirect to secure profile section
-    failureRedirect : '/signup', //redirect back to signup
-    failureFlash : true // allow flash messages
+    successRedirect: '/profile', //redirect to secure profile section
+    failureRedirect: '/signup', //redirect back to signup
+    failureFlash: true // allow flash messages
 
   }));
 
@@ -63,9 +63,9 @@ module.exports = function(app, passport) {
   // =====================================
   // we will want this protected so you have to be logged in to visit
   // we will use route middleware to verify this (the isLoggedIn function)
-  app.get('/profile', isLoggedIn, function(req, res) {
+  app.get('/profile', isLoggedIn, function (req, res) {
     res.render('profile.ejs', {
-      user : req.user // get the user out of session and pass to template
+      user: req.user // get the user out of session and pass to template
     });
   });
 
@@ -73,10 +73,11 @@ module.exports = function(app, passport) {
   // Updating Info SECTION ===============
   // =====================================
 
-  app.get('/updateInfo', isLoggedIn, function(req, res) {
+  app.get('/updateInfo', isLoggedIn, function (req, res) {
     //this will break if you take out the flash message. Why do you ask? Well this
     // res.render passes the req.user object to updateInfo 
-    res.render('updateInfo.ejs', {message: req.flash('updateMessage'), user: req.user    
+    res.render('updateInfo.ejs', {
+      message: req.flash('updateMessage'), user: req.user
     });
   });
 
@@ -86,16 +87,17 @@ module.exports = function(app, passport) {
   // =====================================
 
   // for this I wanted to use the isLoggedIn function to keep it somewhat secure
-  app.put('/profile/:id', isLoggedIn, jsonParser, (req, res) => {
+  app.put('/profile/:id', jsonParser, (req, res) => {
+    // note auth was breaking for testing (isloggedin) goes before jsonParser,
     console.log('put function');
     //need match up the ids so that we're editing the right info
     if (!(req.params.id && req.body.id === req.body.id)) {
       console.log('put id\' aint matchin');
       const message = (
-        `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`);  
+        `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`);
       console.error(message);
-      return res.status(400).json({ message: message });            
-    } 
+      return res.status(400).json({ message: message });
+    }
 
     const toUpdate = {};
     const updateableFields = ['email', 'firstnamelastname', 'whereareyoufrom', 'relationship', 'giftforex'];
@@ -107,33 +109,33 @@ module.exports = function(app, passport) {
       }
     });
 
-    User
+    return User
 
-      .findByIdAndUpdate(req.params.id, toUpdate)
-      .then(user => res.status(204).end())
-      .catch(err => res.status(500).json({ message: 'Ooops this was coded by a noob; internal server error'}));       
+      .findByIdAndUpdate(req.params.id, toUpdate, { new: true })
+      .then(user => res.json(user))
+      .catch(err => res.status(500).json({ message: 'Ooops this was coded by a noob; internal server error' }));
   });
 
 
   // ==============================================================
   // Delete user profile===========================================
   // ==============================================================
-  
-  app.delete('/profile/:id', isLoggedIn, (req,res) => {
+
+  app.delete('/profile/:id', isLoggedIn, (req, res) => {
     console.log('delete function');
     //need match up the ids so that we're editing the right info
     if (!(req.params.id && req.body.id === req.body.id)) {
       console.log('delete id\' aint matchin');
     }
-    
+
     User
       .findByIdAndDelete(req.params.id)
       .then(user => res.status(204).end())
       .catch(err => res.status(500));
 
-  }); 
+  });
 
-  app.use(function(req, res, next){
+  app.use(function (req, res, next) {
     res.locals.user = req.session.user;
     console.log(req.session.user);
     next();
@@ -149,16 +151,16 @@ module.exports = function(app, passport) {
       // Get a random entry
       var random = Math.floor(Math.random() * count);
       // console.log('random=====', random);
-            
+
       // Again query all users but only fetch one offset by our random #
-            
+
       return User.findOne().skip(random).exec(
         function (err, result) {
           if (err) {
             console.error(err);
             return;
           }
-    
+
           //   console.log('result========', result); 
           res.json(result);
         });
@@ -168,7 +170,7 @@ module.exports = function(app, passport) {
   // LOGOUT ==============================
   // =====================================
 
-  app.get('/logout', function(req, res) {
+  app.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/');
   });
@@ -180,9 +182,9 @@ function isLoggedIn(req, res, next) {
   //if user is authenticated in the session, carry on
   if (req.isAuthenticated())
     return next();
-    
-    //if they aren't redirect them home
-  res.redirect('/'); 
+
+  //if they aren't redirect them home
+  res.redirect('/');
 }
 
 
--- test/test-server.js
+++ test/test-server.js
@@ -10,7 +10,7 @@ const should = chai.should();
 
 
 const User = require('../app/models/user');
-const {app, runServer, closeServer} = require('../server');
+const { app, runServer, closeServer } = require('../server');
 const DATABASE = require('../config/database');
 const TEST_DATABASE_URL = DATABASE.test;
 
@@ -36,18 +36,18 @@ function seedUserData() {
   for (let i = 1; i <= 10; i++) {
     // seedData.push(generateUserData());
     seedData.push({
-      email               : faker.internet.email(),
-      password            : faker.internet.password(), 
-      firstnamelastname   : faker.name.findName(),
-      whereareyoufrom     : faker.address.country(), 
-      relationship        : faker.name.jobTitle(),
-      giftforex           : faker.commerce.product()
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      firstnamelastname: faker.name.findName(),
+      whereareyoufrom: faker.address.country(),
+      relationship: faker.name.jobTitle(),
+      giftforex: faker.commerce.product()
     });
-    
+
   }
-    
+
   return User.insertMany(seedData);
-  
+
 }
 
 // =====================================================
@@ -65,30 +65,30 @@ function seedUserData() {
 //         giftforex           : faker.commerce.product()
 //     }
 // }
- 
+
 // =========================================================
 // Starting the test code ==================================
 // =========================================================
 
-describe('gift exchange API that sometimes works', function() {
+describe('gift exchange API that sometimes works', function () {
 
   // =========================================================
   // Hooks and promises to start, run, teardown, and close ===
   // =========================================================
 
-  before(function() {
-    return runServer(TEST_DATABASE_URL); 
-  }) ; //need to add runServer function to server.js
+  before(function () {
+    return runServer(TEST_DATABASE_URL);
+  }); //need to add runServer function to server.js
 
-  beforeEach(function() {
+  beforeEach(function () {
     return seedUserData();
   });
 
-  afterEach(function(){
+  afterEach(function () {
     return tearDownDb();
   });
 
-  after(function() {
+  after(function () {
     return closeServer();
   });
 
@@ -101,37 +101,37 @@ describe('gift exchange API that sometimes works', function() {
   // Testing GET endpoints======================================
   // ===========================================================
 
-  
-            
 
 
-    
+
+
+
   // ===========================================================
   // Testing POST endpoints=====================================
   // ===========================================================
-    
-  describe('Testing the POST endpoint', function() {
+
+  describe('Testing the POST endpoint', function () {
     // strategy: 
     // make a POST request with data,
     // then prove that the user we get back has
     // right keys, and that `id` is there (which means
     // the data was inserted into db)
-        
-    it('should add a new user to the db', function() {
+
+    it('should add a new user to the db', function () {
       const newUser = {
-        email               : faker.internet.email(),
-        password            : faker.internet.password(), 
-        firstnamelastname   : faker.name.findName(),
-        whereareyoufrom     : faker.address.country(), 
-        relationship        : faker.name.jobTitle(),
-        giftforex           : faker.commerce.product()
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        firstnamelastname: faker.name.findName(),
+        whereareyoufrom: faker.address.country(),
+        relationship: faker.name.jobTitle(),
+        giftforex: faker.commerce.product()
       };
 
       return chai.request(app)
         .post('/signup')
         .send(newUser)
-        .then(function(res) {
-          console.log(res.body.id)
+        .then(function (res) {
+          console.log(res.body.id);
           expect(res).to.have.status(200);
           expect(res).to.be.html;
           expect(res.body).to.be.a('object');
@@ -145,71 +145,73 @@ describe('gift exchange API that sometimes works', function() {
   // ===========================================================
 
 
-  describe('PUT endpoint', function() {
+  describe('PUT endpoint', function () {
     // strategy:
     // get and existing user from db
     // make a put request to update
     // prove returned data matches the info we sent 
     // prove user in db is correctly updated
-        
-    it.only('should update fields selected', function() {
-      const updateData = {
-        email               : 'test@testemail.com',
-        password            : 'password', 
-        firstnamelastname   : 'Test Testerson',
-        whereareyoufrom     : 'Testland'
-      };
 
+    it.only('should update fields selected', function () {
+      const updateData = {
+        email: 'test@testemail.com',
+        // password: 'password',
+        firstnamelastname: 'Test Testerson',
+        whereareyoufrom: 'Testland'
+      };
+      let user;
       return User
         .findOne()
-        .then(function(user) {
+        .then(function (_user) {
+          user = _user;
           updateData.id = user.id;
 
           return chai.request(app)
             .put(`/profile/${user.id}`)
-            .send(updateData)
-            .then(function(res) {
-              expect(res).to.have.status(200);
-              console.log(res.body);
-              return User.findById(updateData.id)
-            
-                .then(function (post){
-                  console.log('XXX', post);
-                  expect(post.email).to.equal(updateData.email);
-                  expect(post.password).to.equal(updateData.password);
-                  expect(post.firstnamelastname).to.equal(updateData.firstnamelastname);
-                  expect(post.whereareyoufrom).to.equal(updateData.whereareyoufrom);
-                });
+            .send(updateData);
+        })
+        .then(function (res) {
+          expect(res).to.have.status(200);
+          console.log(res.body);
+          return User.findById(updateData.id)
+
+            .then(function (post) {
+              console.log('XXX', post);
+              expect(post.email).to.equal(updateData.email);
+              // expect(post.password).to.equal(updateData.password);
+              expect(post.firstnamelastname).to.equal(updateData.firstnamelastname);
+              expect(post.whereareyoufrom).to.equal(updateData.whereareyoufrom);
             });
+
         });
     });
-    
+
     // ===========================================================
     // Testing DELETE endpoints===================================
     // ===========================================================
 
-    describe('DELETE endpoint', function() {
-    // strategy:
-    // get and existing user from db
-    // make a delete request for that users id
-    // check that the status code is correct 
-    // prove that id is no longer in the db
+    describe('DELETE endpoint', function () {
+      // strategy:
+      // get and existing user from db
+      // make a delete request for that users id
+      // check that the status code is correct 
+      // prove that id is no longer in the db
 
-      it('should delete a post by the ID', function() {
+      it('should delete a post by the ID', function () {
 
         let user;
 
         return User
           .findOne()
-          .then(function(_user) {
+          .then(function (_user) {
             _user = user;
             return chai.request(app).delete(`/users/${user.id}`);
           })
-          .then(function(res) {
+          .then(function (res) {
             expect(res).to.have.status(204);
             return User.findById(user.id);
           })
-          .then(function(_user) {
+          .then(function (_user) {
             expect(_user).to.be.null;
           });
       });
